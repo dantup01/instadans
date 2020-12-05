6a6e3332301c73969a16a93a6d57f52367fe1610
Daniel Tuppen <daniel.tuppen1@gmail.com>
2020-12-05 20:34:15 +0000

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'home/index'
-
   root to: 'home#index'
 
+  devise_for :users
   resources :users, only: [:show, :edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:new, :create, :show, :destroy]
+  get 'search' => 'search#index'
 end
