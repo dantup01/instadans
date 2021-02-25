6db9c986dc9a8efdb7b95285010104137e9ed297
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-02-25 21:00:23 +0000

    cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+
+Instrument.delete_all
+
+puts 'Adding instruments 🎵🎶 ...'
+
+acoustic_guitar = Instrument.create('acoustic guitar')
+electic_guitar = Instrument.create(name: 'electric guitar')
+bass_guitar = Instrument.create(name: 'bass guitar')
+violin = Instrument.create(name: 'violin')
+viola = Instrument.create(name: 'viola')
+cello = Instrument.create(name: 'cello')
+double_bass = Instrument.create(name: 'double bass')
+banjo = Instrument.create(name: 'banjo')
+mandolin = Instrument.create(name: 'mandolin')
+ukulele = Instrument.create(name: 'ukulele')
+harp = Instrument.create(name: 'harp')
+piano = Instrument.create(name: 'piano')
+keyboard = Instrument.create(name: 'keyboard')
+flute = Instrument.create(name: 'flute')
+clarinet = Instrument.create(name: 'clarinet')
+oboe = Instrument.create(name: 'oboe')
+bassoon = Instrument.create(name: 'bassoon')
+trumpet = Instrument.create(name: 'trumpet')
+trombone = Instrument.create(name: 'trombone')
+french_horn = Instrument.create(name: 'french horn')
+tuba = Instrument.create(name: 'tuba')
+saxophone = Instrument.create(name: 'saxophone')
+drum_hit = Instrument.create(name: 'drum kit')
+bongo_drums = Instrument.create(name: 'bongo drums')
+piano = Instrument.create(name: '')
+
+
+puts 'Finished!'
